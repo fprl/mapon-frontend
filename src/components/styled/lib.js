00384cb015d9c6a7b04231556121736c32e63f57
francoromanolosada <francoromano1994@gmail.com>
2021-06-15 23:43:55 +0200

    Added new components and corrected css variables

--- src/components/styled/lib.js
+++ src/components/styled/lib.js
@@ -19,3 +19,24 @@ export const Grid = styled.div`
   display: grid;
   min-height: 2rem;
 `
+
+export const Title = styled.h1`
+  font-size: var(--text-2xl);
+  font-weight: 400;
+  line-height: 1.2;
+
+  padding: 2.5rem 1.6rem;
+  padding-bottom: 0;
+`
+
+export const Button = styled.button`
+  background-color: var(--color-primary);
+  color: white;
+
+  border: none;
+  border-radius: 3px;
+  padding: 0.5rem 1rem;
+
+  text-align: center;
+  cursor: pointer;
+`
