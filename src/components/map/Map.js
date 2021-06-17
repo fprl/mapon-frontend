f2a2f3b1c8b9d96c5939aa8e97e8deecea35f195
francoromanolosada <francoromano1994@gmail.com>
2021-06-17 20:53:23 +0200

    Deleted center state since not needed

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -10,7 +10,6 @@ const Map = ({ vehicleRoute }) => {
   })
 
   const [map, setMap] = useState(null)
-  const [center, setCenter] = useState({ lat: 56.48009, lng: 23.03112 })
   const [markers, setMarkers] = useState({
     start: {lat: null, lng: null},
     end: {lat: null, lng: null}
@@ -18,6 +17,7 @@ const Map = ({ vehicleRoute }) => {
   const [paths, setPaths] = useState([])
 
   useEffect(() => {
+    // center map
     if (!map) return
     const bounds = new window.google.maps.LatLngBounds()
     for (var i = 0; i < paths.length; i++) {
@@ -33,18 +33,12 @@ const Map = ({ vehicleRoute }) => {
       route => route.type === 'route'
     )
     const firstRoute = routes[0]
-    // const newCenter = { lat: firstRoute.start.lat, lng: firstRoute.start.lng }
     const newPaths = firstRoute.decoded_route.points
-    const middlePath = newPaths[Math.ceil(newPaths.length / 2)]
-    const newCenter = { lat: middlePath.lat, lng: middlePath.lng }
     const newMarkers = {
       start: { lat: firstRoute.start.lat, lng: firstRoute.start.lng },
       end: { lat: firstRoute.end.lat, lng: firstRoute.end.lng },
     }
 
-    setCenter(prevCenter => {
-      return { ...prevCenter, ...newCenter }
-    })
     setMarkers(prevMarkers => {
       return { ...prevMarkers, ...newMarkers}
     })
@@ -54,16 +48,8 @@ const Map = ({ vehicleRoute }) => {
   }, [vehicleRoute])
 
   const onLoad = useCallback(function callback(map) {
-    // setMapRef(map)
-    // this function set the boundaries of the map based on the paths
-    const bounds = new window.google.maps.LatLngBounds()
-    for (var i = 0; i < paths.length; i++) {
-      bounds.extend(paths[i])
-    }
-    map.fitBounds(bounds)
     setMap(map)
-  }, [paths])
-
+  }, [])
 
   const onUnmount = useCallback(function callback(map) {
     setMap(null)
@@ -76,12 +62,11 @@ const Map = ({ vehicleRoute }) => {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={{ width: 'auto', height: '200px' }}
-      center={center}
+      center={{ lat: 0, lng: 0 }}
       zoom={10}
-      // options={mapOptions}
+      options={mapOptions}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      // ref={mapRef}
     >
       <Marker position={markers.start} options={markerOptions} />
       <Marker position={markers.end} options={markerOptions} />
